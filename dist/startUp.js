604ad2f64bdf752bfc2cd6916b29775e52cd2c60
Halysson Freitas <halyssonfreitas@gmail.com>
2022-01-06 19:36:06 -0300

    #add dist/program.js dist/startUp.js arquivos gerados do script compile: tsc -w em package.json derivado de arquivos typescript

---
+++ dist/startUp.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+class StartUp {
+    //digita ctor e enter
+    constructor() {
+        this.app = express();
+        this.routes();
+    }
+    //criando rotas
+    routes() {
+        this.app.route('/').get((req, res) => {
+            res.send({ versao: '0.0.1' });
+        });
+    }
+}
+exports.default = new StartUp();
