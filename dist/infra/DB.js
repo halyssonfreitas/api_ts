d0d17b3d22d91f514d9940674d4db8ac884aa9dc
Halysson Freitas <halyssonfreitas@gmail.com>
2022-01-07 12:36:15 -0300

    #update #bug o DB_URL em DB.ts estava com mongoose, foi corrigido para mongodb

--- dist/infra/DB.js
+++ dist/infra/DB.js
@@ -5,9 +5,10 @@ class DB {
     constructor() {
         // Ideia - Talves posso no construtor pegar dados das variáveis de ambiente ou dados da rede para melhor
         // configurar o DB_URL
-        this.DB_URL = "mongoose://localhost:27017/db_api_ts";
+        this.DB_URL = "mongodb://localhost:27017/db_api_ts";
     }
     createConnection() {
+        console.log(this.DB_URL);
         mongoose.createConnection(this.DB_URL);
     }
 }
--- infra/DB.ts
+++ infra/DB.ts
@@ -3,9 +3,11 @@ import * as mongoose from 'mongoose'
 class DB{
     // Ideia - Talves posso no construtor pegar dados das variáveis de ambiente ou dados da rede para melhor
     // configurar o DB_URL
-    private DB_URL = "mongoose://localhost:27017/db_api_ts"
+    private DB_URL = "mongodb://localhost:27017/db_api_ts"
+
 
     createConnection() {
+        console.log(this.DB_URL)
         mongoose.createConnection(this.DB_URL)
         
     }
