a22440a0cdcb9e39711166eaa89ef65b69d05e6c
Halysson Freitas <halyssonfreitas@gmail.com>
2022-01-07 10:11:57 -0300

    #update services/NewsService.ts e criando os métodos create() update() e delete()

--- dist/services/NewsService.js
+++ dist/services/NewsService.js
@@ -8,5 +8,14 @@ class NewsService {
     getById(_id) {
         return NewsRepository_1.default.findById(_id);
     }
+    create(news) {
+        return NewsRepository_1.default.create(news);
+    }
+    update(_id, news) {
+        return NewsRepository_1.default.findByIdAndUpdate(_id, news);
+    }
+    delete(_id) {
+        return NewsRepository_1.default.findByIdAndRemove(_id);
+    }
 }
 exports.default = new NewsService();
