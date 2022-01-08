7bce5b963f5610b31c33679afdf4d8408677f5c9
Halysson Freitas <halyssonfreitas@gmail.com>
2022-01-08 10:58:09 -0300

    #update - nunca esquecer de rodar o tsc para converter ts em js

--- dist/infra/DB.js
+++ dist/infra/DB.js
@@ -5,12 +5,13 @@ class DB {
     constructor() {
         // Ideia - Talves posso no construtor pegar dados das variáveis de ambiente ou dados da rede para melhor
         // configurar o DB_URL
-        //private DB_URL = "mongodb://link-db/db_api_ts" // Isso não funciona como na video aula
-        // isso é POG, tenho que arrumar uma forma de encontrar o IP do BD
-        // por meio de código, uma vez que, por meio da forma 'mongodb://link-db/db_api_ts' não funciona
-        // este abaixo é apenas o padrão de comportamento que percebi do Docker na geração de IPs, mas nada garantido
-        this.DB_URL = "mongodb://172.18.0.2:27017/db_api_ts";
+        // Para isso funcionar faltava container_name: link_db no docker-compose.yml
+        this.DB_URL = "mongodb://link_db:27017/db_api_ts";
     }
+    // isso é POG, tenho que arrumar uma forma de encontrar o IP do BD
+    // por meio de código, uma vez que, por meio da forma 'mongodb://link-db/db_api_ts' não funciona
+    // este abaixo é apenas o padrão de comportamento que percebi do Docker na geração de IPs, mas nada garantido
+    //private DB_URL = "mongodb://172.18.0.2:27017/db_api_ts"
     createConnection() {
         console.log(this.DB_URL);
         //mongoose.createConnection(this.DB_URL) // esse não funciona, estudar a diferença
